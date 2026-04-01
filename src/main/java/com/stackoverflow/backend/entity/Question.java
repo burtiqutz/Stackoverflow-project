d526189f80e5b7a378b90b16f46efb9cdb7a5d7f
Alex <alex_rock04@yahoo.com>
2026-04-01 15:43:58 +0300

    Fix build error: clean up pom.xml java versions and add missing JPA annotations to Question

--- src/main/java/com/stackoverflow/backend/entity/Question.java
+++ src/main/java/com/stackoverflow/backend/entity/Question.java
@@ -1,8 +1,12 @@
 package com.stackoverflow.backend.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
-
+@Entity
+@Table(name = "answers")
 @Data
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
