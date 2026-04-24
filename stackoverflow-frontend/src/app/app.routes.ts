d3b1dbf46be0773e0623354e7c369100dde36b65
Alex <alex_rock04@yahoo.com>
2026-04-24 17:20:13 +0300

    Add Angular FE skeleton

---
+++ stackoverflow-frontend/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { UserProfile } from './pages/user-profile/user-profile';
+import { QuestionsList } from './pages/questions-list/questions-list';
+import { QuestionDetail } from './pages/question-detail/question-detail';
+
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'profile', component: UserProfile },
+  { path: 'questions', component: QuestionsList },
+  { path: 'questions/:id', component: QuestionDetail },
+  { path: '', redirectTo: '/questions', pathMatch: 'full' }
+];
